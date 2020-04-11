4e73468a94e0e12f0292fb08c2365c73ec0ddfef
Zuojiabin <zuojiabin@MacBook-Pro.lan>
2020-04-11 23:57:55 +0800

    feat(controller): Implement the hello controller for first API.

---
+++ src/main/java/com/togo/accounting/Application.java
@@ -0,0 +1,15 @@
+package com.togo.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @create 2020-04-11 23:37
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ src/main/java/com/togo/accounting/model/Greeting.java
@@ -0,0 +1,10 @@
+package com.togo.accounting.model;
+
+/**
+ * @Description
+ * @create 2020-04-11 23:54
+ **/
+public class Greeting {
+    private Long id;
+    private String name;
+}
