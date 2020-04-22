f63682cf9bc06d8e84cbe0f1d05f3a325a86a25b
Zuojiabin <zuojiabin@MacBook-Pro.lan>
2020-04-22 12:03:55 +0800

    test(environment): Add Junit5 dependency,builded the test environment.

---
+++ src/main/java/com/togo/accounting/manager/AddManager.java
@@ -0,0 +1,10 @@
+package com.togo.accounting.manager;
+
+/**
+ * create by crashLab on 2020/04/22.
+ **/
+public class AddManager {
+    public int add(int number){
+        return number + 1;
+    }
+}
---
+++ src/main/test/java/com/togo/accounting/manager/UserInfoManagerTest.java
@@ -0,0 +1,13 @@
+package com.togo.accounting.manager;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * UserInfoManager Test
+ * create by crashLab on 2020/04/22.
+ **/
+public class UserInfoManagerTest {
+    @Test
+    public void testAddMethod(){
+    }
+}
